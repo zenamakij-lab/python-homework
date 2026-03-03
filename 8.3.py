fb6f2529637d95dd7310795ba2b19d80d0cafd87
zenamakij-lab <zenamakij@gmail.com>
2026-03-03 20:00:19 +0200

    l

---
+++ 8.3.py
@@ -0,0 +1,10 @@
+def find_unique_value(some_list):
+    for num in some_list:
+        if some_list.count(num) == 1:
+            return num
+
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
