ada8ba32fe3d6f54a49b4ec1410c74b3d08aae6e
zenamakij-lab <zenamakij@gmail.com>
2026-03-03 19:54:29 +0200

    7.2

---
+++ 7.2.py
@@ -0,0 +1,17 @@
+def correct_sentence(text):
+
+    text = text[0].upper() + text[1:]
+
+
+    if not text.endswith('.'):
+        text += '.'
+
+    return text
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
