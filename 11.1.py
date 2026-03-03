e25ac3f763ee04977b89d7d3d9545abf66d5203b
zenamakij-lab <zenamakij@gmail.com>
2026-03-03 20:11:05 +0200

    l

---
+++ 11.1.py
@@ -0,0 +1,21 @@
+from inspect import isgenerator
+
+def prime_generator(end):
+
+    for num in range(2, end + 1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield num
+
+
+# Тести
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
