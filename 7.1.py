84e60f9e97304b1c19058eb1ded999d2aa33059c
zenamakij-lab <zenamakij@gmail.com>
2026-03-03 19:42:55 +0200

    7.1

--- 7.1.py
+++ 7.1.py
@@ -1,7 +1,6 @@
 def say_hi(name, age):
-    return "Hi. My name is Женя and I\'m {0} years old".format(str(16))
+    return f"Hi. My name is {name} and I'm {age} years old"
 
-
-assert say_hi("Alex", 32) == "Hi. My name is Женя and I'm 32 years old", 'Test1'
-assert say_hi("Frank", 68) == "Hi. My name is Женя and I'm 68 years old", 'Test2'
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
 print('ОК')
